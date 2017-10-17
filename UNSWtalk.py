bdebe09d31bb4bb48d836769325db9b0c9114bba
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-10-17 14:52:50 +1100

    displaying only safe user info

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -5,6 +5,7 @@
 # https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
 
 import os
+import re
 from flask import Flask, render_template, session
 
 students_dir = "dataset-small";
@@ -24,11 +25,19 @@ def student(z_id):
     # n = session.get('n', 0)
     # students = sorted(os.listdir(students_dir))
     # student_to_show = students[n % len(students)]
+    # session['n'] = n + 1
     details_filename = os.path.join(students_dir, z_id, "student.txt")
     with open(details_filename) as f:
-        details = f.read()
-    # session['n'] = n + 1
-    return render_template('profile.html', student_details=details)
+        details = divideDetailsIntoHash(f.readlines())
+    return render_template('profile.html', details=details, public_attrs=["program", "zid", "birthday", "full_name", "friends"])
+
+def divideDetailsIntoHash(details):
+    hash = {}
+    for line in details:
+        split_string = line.split(':', 1)
+        hash[split_string[0]] = split_string[1]
+    print(hash)
+    return hash
 
 if __name__ == '__main__':
     app.secret_key = os.urandom(12)
