8bc7d5b017bcc3c3097bc71af31d64d0ca2fa848
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-10-18 10:15:52 +1100

    comments

--- database_creator.py
+++ database_creator.py
@@ -4,10 +4,11 @@
 
 import sqlite3, glob, os, re, pathlib, uuid
 import shutil
+# create connection
 conn = sqlite3.connect('database.db')
 conn.text_factory = str
 c = conn.cursor()
-
+# create nescesary tables
 c.execute("DROP TABLE IF EXISTS users")
 c.execute("""CREATE TABLE IF NOT EXISTS users(
 		z_id TEXT PRIMARY KEY NOT NULL,
@@ -50,14 +51,16 @@ c.execute("""CREATE TABLE IF NOT EXISTS replies(
 
 students_dir = "static/dataset-small"
 
+# itterate over each user folder and extract data
 for z_id in sorted(os.listdir(students_dir)):
-	print(z_id)
+	# open the student.txt file
 	with open(os.path.join(students_dir, z_id, "student.txt")) as f:
 		user_details = f.readlines()
 	#get user image or set to default
 	image_path = os.path.join(students_dir, z_id, "img.jpg")
 	if not pathlib.Path(image_path).is_file():
 		image_path = "static/images/defaultprofile.png"
+	# extract user profile data
 	for line in user_details:
 		if 'name: ' in line:
 			name = line.split(': ')[1]
@@ -80,18 +83,18 @@ for z_id in sorted(os.listdir(students_dir)):
 			friends = friends.group(1)
 		if 'courses: ' in line:
 			courses = line
-
+	#store in database
 	c.execute("INSERT INTO users (z_id, name, program, birthday, suburb, email, password, image_path, latitude, longitude, friends, courses) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (z_id, name, program, birthday, suburb, email, password, image_path, home_latitude, home_longitude, friends, courses))
 
+	# start at 0 and count up looking for posts, comments and replies
 	post_counter = 0
 	# set path to post: static/dataset-small/z5191824/x.txt
 	while pathlib.Path(os.path.join(students_dir, z_id, "%d.txt" % post_counter)).is_file():
-		# print("found post %d" % post_counter)
 		comment_counter = 0
 		# open the post, and store in db
 		with open(os.path.join(students_dir, z_id, "%d.txt" % post_counter)) as f:
 			post = f.readlines()
-		# create posts
+		# extract data and save to database
 		for line in post:
 			if 'z_id: ' in line:
 				z_id = line.split(': ')[1]
@@ -103,9 +106,11 @@ for z_id in sorted(os.listdir(students_dir)):
 				latitude = line.split(': ')[1]
 			if 'longitude: ' in line:
 				longitude = line.split(': ')[1]
+		#generate uuid
 		post_id = str(uuid.uuid4()).replace('-','');
 		c.execute("INSERT INTO posts (id, sender, created_at, message, latitude, longitude, path) VALUES (?, ?, ?, ?, ?, ?, ?)", (post_id, z_id, created_at, message, latitude, longitude, os.path.join(students_dir, z_id, "%d.txt" % post_counter)))
 
+		# look for comments on post
 		# set path to comment: static/dataset-small/z5191824/x-y.txt
 		while pathlib.Path(os.path.join(students_dir, z_id, "%d-%d.txt" % (post_counter, comment_counter))).is_file():
 			reply_counter = 0
@@ -123,7 +128,7 @@ for z_id in sorted(os.listdir(students_dir)):
 			comment_id = str(uuid.uuid4()).replace('-','');
 			c.execute("INSERT INTO comments (id, post, sender, created_at, message, path) VALUES (?, ?, ?, ?, ?, ?)", (comment_id, post_id, z_id, created_at, message,os.path.join(students_dir, z_id, "%d-%d.txt" % (post_counter, comment_counter))))
 
-
+			# look for replies on comment
 			# set path to comment: static/dataset-small/z5191824/x-y-z.txt
 			while pathlib.Path(os.path.join(students_dir, z_id, "%d-%d-%d.txt" % (post_counter, comment_counter, reply_counter))).is_file():
 				# open the reply, and store in db
@@ -138,15 +143,12 @@ for z_id in sorted(os.listdir(students_dir)):
 						message = line.split(': ')[1]
 				reply_id = str(uuid.uuid4()).replace('-','');
 				c.execute("INSERT INTO replies (id, comment, sender, created_at, message, path) VALUES (?, ?, ?, ?, ?, ?)", (reply_id, comment_id, z_id, created_at, message,os.path.join(students_dir, z_id, "%d-%d-%d.txt" % (post_counter, comment_counter, reply_counter))))
-
 				reply_counter+=1
 			comment_counter+=1
 		post_counter+=1
 
 
-
-
-
+#commit and close connection
 conn.commit()
 c.close()
 conn.close()
