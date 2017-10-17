a82c87b2c9093af1ee87c44bd5971368bb87036a
z5019999 <z5019999@cse.unsw.edu.au>
2017-10-17 12:08:06 +1100

    first commit

---
+++ UNSWtalk.py
@@ -0,0 +1,31 @@
+#!/web/cs2041/bin/python3.6.3
+
+# written by andrewt@cse.unsw.edu.au October 2017
+# as a starting point for COMP[29]041 assignment 2
+# https://cgi.cse.unsw.edu.au/~cs2041/assignments/UNSWtalk/
+
+import os
+from flask import Flask, render_template, session
+
+students_dir = "dataset-medium";
+
+app = Flask(__name__)
+
+# Show unformatted details for student "n"
+# Increment n and store it in the session cookie
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/start', methods=['GET','POST'])
+def start():
+    n = session.get('n', 0)
+    students = sorted(os.listdir(students_dir))
+    student_to_show = students[n % len(students)]
+    details_filename = os.path.join(students_dir, student_to_show, "student.txt")
+    with open(details_filename) as f:
+        details = f.read()
+    session['n'] = n + 1
+    return render_template('start.html', student_details=details)
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
+    app.run(debug=True)
