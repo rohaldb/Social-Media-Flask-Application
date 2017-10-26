7c60f3a35c90c7dabd555b6a5669c4c847261464
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-10-26 11:08:54 +1100

    home styled

--- UNSWtalk.py
+++ UNSWtalk.py
@@ -19,6 +19,12 @@ DATABASE = 'database.db'
 
 app = Flask(__name__)
 
+#used to import CSS files
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
+
 # redirect url when returning from email
 # redirect_url = "http://cgi.cse.unsw.edu.au"
 redirect_url = "http://127.0.0.1:5000/"
